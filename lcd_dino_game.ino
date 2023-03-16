8619e31ee229ffb699369449befdb3e5d813e6f4
Robotics-PEC <111312267+Robotics-PEC@users.noreply.github.com>
2023-03-17 01:12:40 +0530

    added License

--- lcd_dino_game.ino
+++ lcd_dino_game.ino
@@ -1,4 +1,29 @@
 /*
+*Robotics Pec
+*robotics@pec.edu.in
+*
+*                    GNU GENERAL PUBLIC LICENSE
+*                       Version 3, 29 June 2007
+*
+*Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+*Everyone is permitted to copy and distribute verbatim copies
+*of this license document, but changing it is not allowed.
+*
+*The GNU General Public License is a free, copyleft license for
+*software and other kinds of works.
+*
+*THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
+*APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
+*HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY
+*OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
+*THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+*PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM
+*IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
+*ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+*/
+
+
+/*
   Dino game on arduino
 
   This sketch Demonstrates how to use a 16x2 lCD display, 
